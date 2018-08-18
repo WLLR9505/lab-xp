7b44d6e7be323b1b41c2abb4cbcfed93bac89253
Weuller Jr. Souza <weuller9505@live.com>
2018-08-18 20:44:58 -0300

    Typescript : Classe

--- typescript/test.ts
+++ typescript/test.ts
@@ -1,1 +1,13 @@
-console.log("Hello TypeScript");
+console.log('Hello TypeScript');
+
+class pontoXY {
+    x : number;
+    y : number;
+    constructor (x : number, y : number) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+var p1 = new pontoXY(5, 9);
+console.log(p1);
