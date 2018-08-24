7c100e02732ca445047cbbd5e64f01333013ebb2
Weuller Jr. Souza <weuller9505@live.com>
2018-08-23 23:03:07 -0300

    C++ : Enum

--- c++/test.cpp
+++ c++/test.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
 int main() {
+    enum estado { normal, alerta, perigo }; //0 1 2
+
     std::cout << "Hello C++\n";
+
+    if (perigo == 2) {
+        std::cout << "perigo\n";
+    }
     return 0;
 }
