71052571a2f84ebc78afb580fab979bf66690ac7
Weuller Jr. Souza <weuller9505@live.com>
2018-10-14 16:10:59 -0300

    Plano Cartesiano

--- ruby/test.rb
+++ ruby/test.rb
@@ -1,24 +1,51 @@
 puts("Hello Ruby");
 
-i = gets.to_i;
-# Input de inteiro
-if (i == 0) then
-    puts("ZERO");
-elsif (i == 1) then
-    puts("UM")
-else
-    puts("OUTRO")
+
+def desenharPlano (px, py)
+    puts("\n\n")
+    l = 10
+    c = 0
+    while (l > 0) #linhas
+        printf("#{l}")
+        if (py == l )
+            pos = 0
+            while (pos != px)   #alinha o ponto na horizontal
+                pos += 1
+                printf("    ")
+                if (pos == px)
+                    printf("*")
+                end
+            end
+        end
+        puts("\n|")
+        l -= 1
+    end
+    while (c <= 10) #colunas
+        printf("#{c} - ")
+        c += 1
+    end
+    puts("\n\nX:#{px}   Y:#{py}")
 end
 
 class PontoXY
     def set (x, y)
-        @x = x
-        @y = y
+        if ((x > 0 && x <= 10) && (y > 0 && y <= 10))
+            @x = x
+            @y = y
+        else
+            puts("Fora da escala")
+        end
     end
-    def showPoints
-        puts("x: #{@x}   y: #{@y}")
+    def mostrarPontos
+        puts("\nx: #{@x}   y: #{@y}")
+    end
+    def mostrarPlano
+        desenharPlano(@x, @y)
     end
 end
+
 p1 = PontoXY.new
-p1.set(5, 9)
-p1.showPoints()
+puts("digite dois numeros ( 0  > n <= 10 ) :: pressione ENTER entre cada numero")
+p1.set(gets.to_i, gets.to_i)
+
+p1.mostrarPlano()
